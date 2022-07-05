60cfefa92d64909609f76aea0eeb00b2ecda53f5
eleister <54188356+eleister@users.noreply.github.com>
2022-07-05 09:23:44 -0700

    Add files via upload

---
+++ disappear.js
@@ -0,0 +1,9 @@
+AFRAME.registerComponent ('disappear', {
+    init: function () {
+
+let disappearfunc = () => {
+   begin.el.setAttribute ("visible", true);
+    };
+
+    this.el.addEventListener('click', "visible", false,);
+}});
